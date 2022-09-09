a58e55483e22477cdc738ee0338fa572f0bea727
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2022-09-09 09:22:33 +0300

    usenavigate

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -4,6 +4,8 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import axios from "axios";
 import Read from "./read";
+import StateContext from "../context/stateContext";
+
 const InputTodo = () => {
   const apikey = process.env.REACT_APP_API_KEY;
   const [show, setShow] = useState(false);
@@ -21,43 +23,57 @@ const InputTodo = () => {
     axios.put(`${apikey}${id}`, {
       todo,
     });
+
+    alert(id);
+  };
+  const localdata = () => {
+    localStorage.setItem("ID", id);
+    alert(id);
   };
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Change ToDo
-      </Button>
+      <StateContext.Provider value={todo}>
+        <Button
+          variant="primary"
+          onClick={function (event) {
+            handleShow();
+            localdata();
+          }}
+        >
+          Add/Change ToDo
+        </Button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>ToDo Creat/Change</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Input:</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={todo}
-                onChange={(e) => settodoList(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={updateAPIData}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>ToDo Creat/Change</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Input:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={todo}
+                  onChange={(e) => settodoList(e.target.value)}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={updateAPIData}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </StateContext.Provider>
     </>
   );
 };
